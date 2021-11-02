2e1aa7889fe44c5ffd9ffd89ecccaba49a4adaa7
MNosse <80108925+MNosse@users.noreply.github.com>
2021-11-01 21:18:50 -0300

    adicionando styled

--- App.tsx
+++ App.tsx
@@ -1,21 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as S from './styles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <S.Container>
+      <S.CenterLabel>
+        Open up App.tsx to start working on your app!
+      </S.CenterLabel>
       <StatusBar style="auto" />
-    </View>
+    </S.Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #000000;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CenterLabel = styled.Text`
+  color: #ff69ff;
+`;
