9448f174e1e68e4b06975692435dc9d8d41b6fb7
MNosse <80108925+MNosse@users.noreply.github.com>
2021-11-05 20:31:16 -0300

    inicio tela saldo

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import LoginScreen from './modules/Autentication/Login/pages/LoginScreen';
+import OpeningBalanceScreen from './modules/Autentication/SignIn/pages/OpeningBalanceScreen';
 import HomeScreen from './modules/Main/pages/HomeScreen';
 
 export default function App() {
-  return <HomeScreen />;
+  return <OpeningBalanceScreen />;
 }
